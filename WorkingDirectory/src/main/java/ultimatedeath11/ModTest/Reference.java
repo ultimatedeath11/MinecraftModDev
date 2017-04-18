6fda6034ff58956fc98db50cc430b27ea2669512
Noah Franck <ultimatedeath11@icloud.com>
2017-04-18 18:40:55 -0500

    added acceptable version of Minecraft

--- WorkingDirectory/src/main/java/ultimatedeath11/ModTest/Reference.java
+++ WorkingDirectory/src/main/java/ultimatedeath11/ModTest/Reference.java
@@ -4,6 +4,7 @@ public class Reference {
 	public static final String ModID = "UD11TestMod";
 	public static final String Name = "Ultimatedeath11's test Mod";
 	public static final String Version = "1.0.0";
+	public static final String AcceptedVersions = "[1.10.2]";
 	
 	public static final String clientProxyClass = "";
 	public static final String ServerProxyClass = "";
