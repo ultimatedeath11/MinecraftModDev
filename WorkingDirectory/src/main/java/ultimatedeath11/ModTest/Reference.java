174b9c49738f4c09bea09e54f08ef889a8a4eef7
Noah Franck <ultimatedeath11@icloud.com>
2017-04-18 18:32:34 -0500

    Created mod's refrence
    so these are final variables that can’t be edited at all.
    
    I gave it an ID, name, version name, but I also gave it client and
    server classes, but I don’t know what od do with that one yet.

---
+++ WorkingDirectory/src/main/java/ultimatedeath11/ModTest/Reference.java
@@ -0,0 +1,10 @@
+package ultimatedeath11.ModTest;
+
+public class Reference {
+	public static final String ModID = "UD11TestMod";
+	public static final String Name = "Ultimatedeath11's test Mod";
+	public static final String Version = "1.0.0";
+	
+	public static final String clientProxyClass = "";
+	public static final String ServerProxyClass = "";
+}
