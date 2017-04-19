0c0772e09b7bf932a8b31458dbc5f0975f9f1140
Noah Franck <ultimatedeath11@icloud.com>
2017-04-18 20:11:00 -0500

    The tutorial file
    I have added a bunch to it with a lot of ‘@<>’ that I can’t seem to get
    to work, and it is upsetting me greatly that I can not get them to
    work.

--- WorkingDirectory/src/main/java/ultimatedeath11/ModTest/Tutorial.java
+++ WorkingDirectory/src/main/java/ultimatedeath11/ModTest/Tutorial.java
@@ -1,5 +1,25 @@
 package ultimatedeath11.ModTest;
 
+import net.minecraftforge.fml.common.Mod;
+
+
+
+@Mod(modid = Reference.ModID, name = Reference.Name, version = Reference.Version, AcceptedMinecraftVersion = Reference.AcceptedVersions)
 public class Tutorial {
 
+	@Instance
+	public static Tutorial instance;
+	
+	@sidedProxy(clientSide = Reference.clientProxyClass, serverSide = Reference.ServerProxyClass)
+	public static CommonProxy proxy;
+	
+	@EventHandler
+	public void preInit(FMLPreInitilazationEvent event){
+		
+	})
+	
+	
+	
+	
+	
 }
